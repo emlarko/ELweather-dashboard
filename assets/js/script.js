d9dae61c8854f568183d4e6509585e187daf0d66
Emily Larkin <emilylarkin@Emilys-MacBook-Air.local>
2023-01-29 11:50:24 +0000

    API geo location

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,66 @@
 var APIKey = "5e5c2d650b810bd48ba1f15cd93707d2"
-var queryUrl = "https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=" + APIKey;
 
-var city = 
+var userFormEl = document.querySelector('#user-form');
+var cityButtonsEl = document.querySelector('#city-buttons');
+var cityInputEl = document.querySelector('#city');
+var weatherContainerEl = document.querySelector('#weather-container');
 
 
-fetch(queryUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var city = cityInputEl.value.trim();
+
+  if (city) {
+    getLocation(city);
+
+    weatherContainerEl.textContent = '';
+    cityInputEl.value = '';
+  } else {
+    alert('Please enter a City');
+  }
+};
+
+
+var buttonClickHandler = function (event) {
+  var city = event.target.getAttribute('data-city');
+
+  if (city) {
+    getLocation(city);
+  
+    weatherContainerEl.textContent = '';
+  }
+  };
+
+
+var getLocation = function (city) {
+  var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&lat=&lon=&appid=" + APIKey;
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayCity(city);
+          console.log(data);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect');
+    });
+};
+
+
+var displayCity = function (city) {
+
+var cityEl = document.createElement('h3');
+cityEl.textContent = city;
+
+weatherContainerEl.appendChild(cityEl);
+
+}
+
+userFormEl.addEventListener('submit', formSubmitHandler);
+cityButtonsEl.addEventListener('click', buttonClickHandler);
